910fb74ec493126dc12916fa9551aa2c88466673
Pallavi Aggarwal <u1009982@hci.utah.edu>
2018-12-12 15:31:30 -0700

    all files committed

---
+++ src/App.test.js
@@ -0,0 +1,19 @@
+import App from './App';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+/*import Enzyme, { shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+
+
+Enzyme.configure({ adapter: new Adapter() });
+*/
+describe('App', () => {
+    it('should be able to run tests', () => {
+        expect(1 + 2).toEqual(3);
+    });
+});
+
+
+
+
